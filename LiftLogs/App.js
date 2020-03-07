e4042fbb48a0085a3ecc820dc748be8716903b45
Usman Siddiqui <usman98789@hotmail.com>
2020-03-07 14:25:57 -0500

    testing ar

--- LiftLogs/App.js
+++ LiftLogs/App.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {
+  ViroARScene,
+  ViroText,
+  ViroConstants,
+  ViroBox,
+  ViroMaterials,
+  Viro3DObject,
+  ViroAmbientLight,
+  ViroSpotLight,
+} from 'react-viro';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Lift Logs</Text>
-    </View>
+  <ViroARScene onTrackingUpdated={this._onInitialized} >
+    <Viro3DObject source={require('./res/object_coffee_mug.vrx')}
+              // resources={[require('./res/texture1.jpg'),
+              //             require('./res/texture2.jpg'),
+              //             require('./res/texture3.jpg')]}
+              position={[0.0, 0.0, -10]}
+              scale={[0.1, 0.1, 0.1]}
+              type="VRX"
+    />
+  </ViroARScene>
   );
 }
 

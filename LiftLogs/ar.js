458a0e8d53cb5c3e4a7ddeb6e3d041c61986b960
Rahmatullah Nikyar <rahmatullah.nikyar@gmail.com>
2020-03-07 20:11:20 -0500

    trying startARSession function

--- LiftLogs/ar.js
+++ LiftLogs/ar.js
@@ -7,13 +7,46 @@ import ExpoTHREE from "expo-three"
 
 
 export default class Ar extends React.Component {
- render() {
-     return(       
-        <GLView           
+  render() {
+    return(       
+      <GLView
+        ref={(ref) => this._glView = ref}
         style={{ flex: 1 }}           
-        onContextCreate={this._onGLContextCreate}/>    
-        );
+        onContextCreate={this._onGLContextCreateAR}/>    
+    );
+  }
+  
+  _onGLContextCreateAr = async(gl) => {
+    const width = gl.drawingBufferWidth;
+    const height = gl.drawingBufferHeight;
+
+    const arSession = await this._glView.startARSessionAsync();
+
+    const scene = new THREE.Scene();
+    const camera = ExpoTHREE.createARCamera(arSession, width, height, 0.01, 1000);
+    const renderer = ExpoTHREE.createRenderer({ gl });
+    renderer.setSize(gl.drawingBufferWidth, gl.drawingBufferHeight);
+
+    scene.background = ExpoTHREE.createARBackgroundTexture(arSession, renderer);
+
+    // Edit the box dimensions here and see changes immediately!
+    const geometry = new THREE.BoxGeometry(0.07, 0.07, 0.07);
+    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    const cube = new THREE.Mesh(geometry, material);
+    cube.position.z = -0.4;
+    scene.add(cube);
+
+    const animate = () => {
+      requestAnimationFrame(animate);
+
+      cube.rotation.x += 0.07;
+      cube.rotation.y += 0.04;
+
+      renderer.render(scene, camera);
+      gl.endFrameEXP();
     }
+    animate();
+  }
 
   _onGLContextCreate = async (gl) => {
     // Here is where we will define our scene, camera and renderer

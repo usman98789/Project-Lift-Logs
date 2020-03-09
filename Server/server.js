7c949751106571e8cb34a3c23dfeacb242695782
jikailong <jikai.long@mail.utoronto.ca>
2020-03-08 17:31:55 -0800

    basic server done, need test

--- Server/server.js
+++ Server/server.js
@@ -42,24 +42,43 @@ MongoClient.connect(url, function(err, db) {
     });
 }); 
 
-//create a collection called data
+//create a collection called routines
 MongoClient.connect(url, function(err, db) {
     if (err) throw err;
     var dbo = db.db("mydb");
-    dbo.createCollection("data", function(err, res) {
+    dbo.createCollection("routines", function(err, res) {
       if (err) throw err;
       console.log("Collection created!");
       db.close();
     });
 }); 
 
+//create a collection called workouts
+MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("mydb");
+    dbo.createCollection("workouts", function(err, res) {
+      if (err) throw err;
+      console.log("Collection created!");
+      db.close();
+    });
+}); 
+
+// create a collection called log
+MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("mydb");
+    dbo.createCollection("log", function(err, res) {
+      if (err) throw err;
+      console.log("Collection created!");
+      db.close();
+    });
+}); 
+
+
+
 var isAuthenticated = function(req, res, next) {
     if (!req.username) return res.status(401).end("access denied");
-    next();
-};
-
-var isAuthenticatedRead = function(req, res, next) {
-    if (!req.username) return res.status(401);
     next();
 };
 
@@ -67,35 +86,36 @@ const cookie = require('cookie');
 
 /*
  the follow is our datastructure
-{ username: "rahmthehandsome",
-  routine : [{name: "chestallday", workouts: [{name: "upperchest", exercises: [{name: "bench", reps: 100, weight: 10kg}, {"another_bench, reps: 5, weight: 50kg"}]}]}]
+ 
+ Routine:{
+     id: something,
+     name: something;
+ }
+
+ Workout: {
+     id: something
+     name: something;
+     exercises: []  <= a list of exercises
+ }
 }
 not working on date yet, we can leave it for later like when a routine is created
 */
 
 // create an user: this is different from authedication user, this one is for datastorage
 
-let User = (function(){
-    return function new_user(user_name){
-        this.user_name = user_name;
-        this.routine = [];
-        this.temp_workout = new Workout("Temp Workout");
-        this.Completed_workout = [];
-    }
-}());
-
 // create a routine 
 let Routine = (function(){
-    return function new_routine(routine_name){
+    return function new_routine(routine_name, user){
         this.routine_name = routine_name;
-        this.workouts = [];
+        this.user = user;
     }
 }());
 
 // create a workout
 let Workout = (function(){
-    return function new_workout(workout_name){
+    return function new_workout(workout_name, routine_id){
         this.workout_name = workout_name;
+        this.routine_id = routine_id;
         this.exercises = [];
     }
 }());
@@ -139,10 +159,6 @@ app.post('/signup/', function (req, res, next) {
             // insert username, password and salt to the database
             dbo.collection("clients").insertOne({_id: username, password: password, salt: salt}, function (err, res) {
                 if (err) return res.status(500).end(err);
-                // push user_name to the data_collection
-                dbo.collection("data").insertOne(new User(username), function(err, result) {
-                    if (err) return res.status(500).end(err);
-                });
                 res.setHeader('Set-Cookie', cookie.serialize('username', username, {
                     path : '/', 
                     maxAge: 60 * 60 * 24 * 7
@@ -176,6 +192,7 @@ app.post('/signin/', function (req, res, next) {
                 maxAge: 60 * 60 * 24 * 7
             }));
             req.session.user = username;
+            db.close();
             return res.json("user " + username + " signed in");
         });
     });
@@ -191,24 +208,189 @@ app.get('/signout/', function (req, res, next) {
 });
 
 
-
-
-/** TODO
 // Creation
 
+
 // create routine
+app.post('/users/routines/',  isAuthenticated, function (req, res, next) {
+    MongoClient.connect(url, function(err, db) {
+    if (err) return res.status(500).end(err);
+    var dbo = db.db("mydb");
+    dbo.collection("routines").insertOne(new Routine(req.body.routine_name, req.session.user), function (err, res) {
+        if (err) return res.status(500).end(err);
+        db.close();
+        return;
+    })
+    })
+});
 
 // create workouts
+app.post('/users/workouts/:id/',  isAuthenticated, function (req, res, next) {
+    MongoClient.connect(url, function(err, db) {
+        if (err) return res.status(500).end(err);
+        var dbo = db.db("mydb");
+        dbo.collection("workouts").insertOne(new Workout(req.body.workout_name, req.params.id), function (err, res) {
+            if (err) return res.status(500).end(err);
+            db.close();
+            return;
+        })
+        })
+});
 
 // create exercises 
-
-// create temp workout
+app.post('/users/exercises/:id/',  isAuthenticated, function (req, res, next) {
+    MongoClient.connect(url, function(err, db) {
+    if (err) return res.status(500).end(err);
+    var dbo = db.db("mydb");
+    dbo.collection("workouts").updateOne(
+        {_id: req.params.id},
+        { $push: { exercises: new Exercise(req.body.exercise_name, req.body.sets, req.body.reps, req.body.weights)} }).done(function (err, updElem) {
+            if (err) return res.status(500).end(err);
+            res.json(updElem);   
+            db.close(); 
+        });
+    })
+});
 
 // Read
 
+// get all routines
+app.get('/users/routines/', isAuthenticated, function(req, res, next){
+    MongoClient.connect(url, function(err, db) {
+    if (err) return res.status(500).end(err);
+    var dbo = db.db("mydb");
+    dbo.collection("routines").find({}, function(err, result) {
+        if (err) throw err;
+        res.json(result);
+        db.close();
+      })
+    });
+});
+
+// get all workouts given a routine
+app.get('/users/workouts/:id/', isAuthenticated, function(req, res, next){
+    MongoClient.connect(url, function(err, db) {
+    if (err) return res.status(500).end(err);
+    var dbo = db.db("mydb");
+    dbo.collection("workouts").find({routine_id: req.params.id}, function(err, result) {
+        if (err) throw err;
+        res.json(result);
+        db.close();
+      })
+    });
+});
+
+// get a specific workout given workout id
+app.get('/users/workouts/:id/:workout_id/', isAuthenticated, function(req, res, next){
+    MongoClient.connect(url, function(err, db) {
+    if (err) return res.status(500).end(err);
+    var dbo = db.db("mydb");
+    dbo.collection("workouts").find({routine_id: req.params.id, _id: req.params.workout_id}, function(err, result) {
+        if (err) throw err;
+        res.json(result);
+        db.close();
+      })
+    });
+});
+
+// get users log
+app.get('/users/log', isAuthenticated, function(req, res, next){
+    MongoClient.connect(url, function(err, db) {
+    if (err) return res.status(500).end(err);
+    var dbo = db.db("mydb");
+    dbo.collection("log").find({user: req.session.user}, null, {sort :{timestamp : -1}}, function(err, result) {
+        if (err) throw err;
+        res.json(result);
+        db.close();
+      })
+    });
+});
+
+// deletion
+
+// delete a routine 
+app.delete('/users/routines/:id', isAuthenticated, function(req, res, next){
+    MongoClient.connect(url, function(err, db) {
+        if (err) return res.status(500).end(err);
+        var dbo = db.db("mydb");
+        dbo.collection("routines").deleteOne({_id: req.params.id}, function(err, obj) {
+            if (err) throw err;
+        });
+        dbo.collection("workouts").deleteMany({routine_id: req.params.id}, function(err, obj) {
+            if (err) throw err;
+        });
+    });
+});
+
+// delete a workout
+app.delete('/users/workouts/:id', isAuthenticated, function(req, res, next){
+    MongoClient.connect(url, function(err, db) {
+        if (err) return res.status(500).end(err);
+        var dbo = db.db("mydb");
+        dbo.collection("workouts").deleteOne({_id: req.params.id}, function(err, obj) {
+            if (err) throw err;
+            db.close();
+        });
+    });
+});
+
+// delete an exercise
+app.delete('/users/exercise/:id/:exercise_id', isAuthenticated, function(req, res, next){
+    MongoClient.connect(url, function(err, db) {
+    if (err) return res.status(500).end(err);
+    var dbo = db.db("mydb");
+    dbo.collection("workouts").update({_id: req.params.id}, {$pull: {exercises: {_id: req.params.exercise_id}}}).done(function (err, updElem) {
+        if (err) return res.status(500).end(err);
+        res.json(updElem);   
+        db.close(); 
+        });
+    });
+});
+
 // write
- 
-**/
+
+// all write function requires a new object in return, we can modify it later, but i dont think there is any efficiency improvement
+
+// modify the routine, the only thing we can modify is its name
+app.patch('/users/routine/:id/', isAuthenticated, function(req, res, next){
+    MongoClient.connect(url, function(err, db) {
+    if (err) return res.status(500).end(err);
+    var dbo = db.db("mydb");
+    dbo.collection("routines").update({_id: req.params.id}, req.body.updated_routine).done(function (err, updElem) {
+        if (err) return res.status(500).end(err);
+        res.json(updElem);   
+        db.close(); 
+        });
+    });
+});
+
+
+
+// modify a workout (replace the existing workout) I dont know if we really need to modify an exercise since we can simply bind the whole workout in the req
+app.patch('/users/workout/:id/', isAuthenticated, function(req, res, next){
+    MongoClient.connect(url, function(err, db) {
+    if (err) return res.status(500).end(err);
+    var dbo = db.db("mydb");
+    dbo.collection("workouts").update({_id: req.params.id}, req.body.updated_workout).done(function (err, updElem) {
+        if (err) return res.status(500).end(err);
+        res.json(updElem);   
+        db.close(); 
+        });
+    });
+});
+
+// add an workout to log along with timestmap
+app.patch('/users/log/', isAuthenticated, function(req, res, next){
+    MongoClient.connect(url, function(err, db) {
+    if (err) return res.status(500).end(err);
+    var dbo = db.db("mydb");
+    dbo.collection("log").insertOne({workout: req.body.workout, user: req.session.user, timestamp: { type: Date, default: Date.now}}, function (err, res) {
+        if (err) return res.status(500).end(err);
+        db.close();
+        })
+    });
+});
+
 
 const http = require('http');
 const PORT = 3000;

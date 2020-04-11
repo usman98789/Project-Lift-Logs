34f56d97ea840cc90a535693229f4902c9b7f2f3
jikailong <jikai.long@mail.utoronto.ca>
2020-04-11 10:36:41 -0700

    model works

--- Server/server.js
+++ Server/server.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const crypto = require('crypto');
 const session = require('express-session');
 const mongo = require('mongodb'); 
+const tf = require('@tensorflow/tfjs');
 
 
 app.use(session({
@@ -185,6 +186,7 @@ app.post('/signup/', function (req, res, next) {
 });
 
 app.post('/signin/', function (req, res, next) {
+
     var username = req.body.username;
     // retrieve user from the database
         MongoClient.connect(url, function(err, db) {
@@ -425,9 +427,243 @@ app.patch('/users/log/', isAuthenticated, function(req, res, next){
     });
 });
 
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// the following is for tensorflow training
+// data process 
+// util function to normalize a value between a given range.
+
+function extractlog(workouts){
+    let accum_bench_set = 0;
+    let accum_bench_rep = 0;
+    let accum_bench_weight = 0;
+    let accum_squat_set = 0;
+    let accum_squat_rep = 0;
+    let accum_squat_weight = 0;
+    let accum_deadlift_set = 0;
+    let accum_deadlift_rep = 0;
+    let accum_deadlift_weight = 0;
+    let curr_exercise;
+    let count_bench = 0;
+    let count_squat = 0;
+    let count_deadlift = 0;
+    for(i = 0; i < workouts.length; i++){
+        for(k = 0; k < workouts[i].exercises.length; k++){
+            curr_exercise = workouts[i].exercises[k];
+            if(curr_exercise.exercise_name == "bench" || curr_exercise.exercise_name == "bench press"){
+                accum_bench_set = accum_bench_set + curr_exercise.sets;
+                accum_bench_rep = accum_bench_rep + curr_exercise.reps;
+                accum_bench_weight = accum_bench_weight + curr_exercise.weights.match(/\d+/g).map(Number);
+                count_bench++;
+            }
+            else if(curr_exercise.exercise_name == "squat" || curr_exercise.exercise_name == "leg press"){
+                accum_squat_set = accum_squat_set + curr_exercise.sets;
+                accum_squat_rep = accum_squat_rep + curr_exercise.reps;
+                accum_squat_weight = accum_squat_weight + curr_exercise.weights.match(/\d+/g).map(Number);
+                count_squat++;
+            }
+            else if(curr_exercise.exercise_name == "deadlift" || curr_exercise.exercise_name == "dead lift"){
+                accum_deadlift_set = accum_deadlift_set + curr_exercise.sets;
+                accum_deadlift_rep = accum_deadlift_rep + curr_exercise.reps;
+                accum_deadlift_weight = accum_deadlift_weight + curr_exercise.weights.match(/\d+/g).map(Number);
+                count_deadlift++;
+            }
+        }
+    }
+
+    if(count_bench == 0) {count_bench = 1}
+    if(count_deadlift == 0) {count_deadlift = 1}
+    if(count_squat == 0) {count_squat = 1}
+    console.log([accum_bench_rep/count_bench, accum_bench_set/count_bench, accum_bench_weight/count_bench,
+        accum_squat_rep/count_squat, accum_squat_set/count_squat, accum_squat_weight/count_squat,
+        accum_deadlift_rep/count_deadlift, accum_deadlift_set/count_deadlift, accum_deadlift_weight/count_deadlift])
+    return [accum_bench_rep/count_bench, accum_bench_set/count_bench, accum_bench_weight/count_bench,
+            accum_squat_rep/count_squat, accum_squat_set/count_squat, accum_squat_weight/count_squat,
+            accum_deadlift_rep/count_deadlift, accum_deadlift_set/count_deadlift, accum_deadlift_weight/count_deadlift]
+
+    
+}
 
 
 
+app.get('/users/recommendation/',isAuthenticated, function(req, res, next){
+    //test_sample = [3, 3, 200, 2, 4, 200, 2, 2, 200];
+    MongoClient.connect(url, function(err, db) {
+        if (err) return res.status(500).end(err);
+        var dbo = db.db("mydb");
+        dbo.collection("log").find({user: req.session.user}).toArray(function(err, result) {
+            if (err) throw err;
+            recommend = predictSample(extractlog(result))
+            recommend.then(function(myresult){
+                res.json(myresult)
+            })
+            db.close();
+          })
+        });
+});
+
+
+
+
+function normalize(value, min, max) {
+    if (min === undefined || max === undefined) {
+      return value;
+    }
+    return (value - min) / (max - min);
+}
+
+// define timeout during training
+const TIMEOUT_BETWEEN_EPOCHS_MS = 500;
+
+// define data path
+const TEST_DATA_PATH = 'https://storage.googleapis.com/liftlog/testing_data.csv';
+const TRAIN_DATA_PATH =  'https://storage.googleapis.com/liftlog/training_data.csv';
+// Constants from training data
+const rep_MIN = 0;
+const rep_MAX = 80;
+const set_MIN = 0;
+const set_MAX = 20;
+const weight_MIN = 0;
+const weight_MAX = 800; 
+const NUM_CLASS = 3;
+const TRAINING_DATA_LENGTH = 24;
+const TEST_DATA_LENGTH = 6;
+// Converts a row from the CSV into features and labels.
+// Each feature field is normalized within training data constants
+const csvTransform =
+    ({xs, ys}) => {
+      const values = [
+        normalize(xs.bench_rep, rep_MIN, rep_MAX),
+        normalize(xs.bench_sets, set_MIN, set_MAX),
+        normalize(xs.bench_weight, weight_MIN, weight_MAX), 
+        normalize(xs.squat_rep, rep_MIN, rep_MAX),
+        normalize(xs.squat_set, set_MIN, set_MAX),
+        normalize(xs.squat_weight, weight_MIN, weight_MAX), 
+        normalize(xs.deadlift_rep, rep_MIN, rep_MAX),
+        normalize(xs.deadlift_set, set_MIN, set_MAX),
+        normalize(xs.deadlift_weight, weight_MIN, weight_MAX), 
+      ];
+      return {xs: values, ys: ys.target};
+}
+
+
+const trainingData =
+    tf.data.csv(TRAIN_DATA_PATH, {columnConfigs: {target: {isLabel: true}}})
+        .map(csvTransform)
+        .shuffle(TRAINING_DATA_LENGTH)
+        .batch(8);
+
+// Load all training data in one batch to use for evaluation
+const trainingValidationData =
+    tf.data.csv(TRAIN_DATA_PATH, {columnConfigs: {target: {isLabel: true}}})
+        .map(csvTransform)
+        .batch(TRAINING_DATA_LENGTH);
+
+// Load all test data in one batch to use for evaluation
+const testValidationData =
+    tf.data.csv(TEST_DATA_PATH, {columnConfigs: {target: {isLabel: true}}})
+        .map(csvTransform)
+        .batch(TEST_DATA_LENGTH);
+
+// add model
+// two layer feed forward neural network 
+const model = tf.sequential();
+model.add(tf.layers.dense({units: 120, activation: 'relu', inputShape: [9]}));
+model.add(tf.layers.dense({units: 75, activation: 'relu'}));
+model.add(tf.layers.dense({units: NUM_CLASS, activation: 'softmax'}));
+
+model.compile({
+  optimizer: tf.train.adam(),
+  loss: 'sparseCategoricalCrossentropy',
+  metrics: ['accuracy']
+});
+
+// Returns pitch class evaluation percentages for training data 
+// with an option to include test data
+async function evaluate(useTestData) {
+    let results = {};
+    await trainingValidationData.forEachAsync(exerciseTypeBatch => {
+      const values = model.predict(exerciseTypeBatch.xs).dataSync();
+      const classSize = ~~ TRAINING_DATA_LENGTH / NUM_CLASS;
+      for (let i = 0; i < NUM_CLASS; i++) {
+        results[ExerciseFromClassNum(i)] = {
+          training: calcExerciseClassEval(i, classSize, values)
+        };
+      }
+    });
+  
+    if (useTestData) {
+      await testValidationData.forEachAsync(exerciseTypeBatch => {
+        const values = model.predict(exerciseTypeBatch.xs).dataSync();
+        const classSize = ~~ TEST_DATA_LENGTH / NUM_CLASS;
+        for (let i = 0; i < NUM_CLASS; i++) {
+          results[ExerciseFromClassNum(i)].validation =
+              calcExerciseClassEval(i, classSize, values);
+        }
+      });
+    }
+    return results;
+}
+  
+async function predictSample(sample) {
+    let result = model.predict(tf.tensor(sample, [1,sample.length])).arraySync();
+    var maxValue = 0;
+    for (var i = 0; i < NUM_CLASS; i++) {
+      if (result[0][i] > maxValue) {
+        predicted_exe = i;
+        maxValue = result[0][i];
+      }
+    }
+
+    return ExerciseFromClassNum(predicted_exe);
+}
+  
+  // Determines accuracy evaluation for a given pitch class by index
+function calcExerciseClassEval(exerciseIndex, classSize, values) {
+    // Output has 3 different class values for each pitch, offset based on
+    // which pitch class (ordered by i)
+    let index = (exerciseIndex * classSize * NUM_CLASS) + exerciseIndex;
+    let total = 0;
+    for (let i = 0; i < classSize; i++) {
+      total += values[index];
+      index += NUM_CLASS;
+    }
+    return total / classSize;
+}
+  
+  // Returns the string value for Baseball pitch labels
+function ExerciseFromClassNum(classNum) {
+    switch (classNum) {
+      case 0:
+        return 'bench';
+      case 1:
+        return 'squat';
+      case 2:
+        return 'deadlift';
+      case 3:
+        return 'null';
+    }
+}
+
+// boost the training 
+// util function to sleep for a given ms
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+ 
+async function run() {
+    
+    let numTrainingIterations = 5;
+    for (var i = 0; i < numTrainingIterations; i++) {
+        console.log(`Training iteration : ${i+1} / ${numTrainingIterations}`);
+        await model.fitDataset(trainingData, {epochs: 1});
+        console.log('accuracyPerClass', await evaluate(true));
+        await sleep(TIMEOUT_BETWEEN_EPOCHS_MS);
+    }
+    
+}
+run();
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 const http = require('http');
 const PORT = 3000;
 
@@ -435,3 +671,5 @@ http.createServer(app).listen(PORT, function (err) {
     if (err) console.log(err);
     else console.log("HTTP server on http://localhost:%s", PORT);
 });
+
+

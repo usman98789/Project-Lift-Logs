368e652a8b076a4da312fa11e6c7de5f2e1daf6f
Rahmatullah Nikyar <rahmatullah.nikyar@gmail.com>
2020-04-11 23:20:55 -0400

    fixed bug with user charts

--- LiftLogs/components/Charts/UserCharts.js
+++ LiftLogs/components/Charts/UserCharts.js
@@ -4,14 +4,14 @@ import { Button } from "react-native-elements";
 import { LineChart } from "react-native-chart-kit";
 
 const UserCharts = props => {
-  const [userExercises, setUserExercises] = useState(["bench", "shoulderpress"]);
-  const [selectedEx, setSelectedEx] = useState(userExercises[0] || null);
+  const [userExercises, setUserExercises] = useState(null);
+  const [selectedEx, setSelectedEx] = useState(null);
   const [selectedExWeight, setSelectedExWeight] = useState([]);
 
-  const getExercises = () => {
+  const getExercises = async () => {
     // for using your physical phone, add your ip address
     let localIPAddress = "";
-    fetch(`http://${localIPAddress}:3000/users/log`, {
+    await fetch(`http://${localIPAddress}:3000/users/log`, {
       method: "GET",
       headers: {
         Accept: "application/json",
@@ -21,19 +21,15 @@ const UserCharts = props => {
     })
       .then(resJson => resJson.json())
       .then(logs => {
-        // console.log({ "res": res });
         let exerciseList = [];
         logs.map(workout => {
           workout.exercises.map(exercise => {
             exerciseList.push(exercise.exName);
           });
         })
-        console.log({ "exerciselist": exerciseList });
-        setUserExercises([... new Set(exerciseList)]);
-      })
-      .then(() => {
-        // also set the default value for selectedEx
-        setSelectedEx(userExercises[0]);
+        let temp = [... new Set(exerciseList)];
+        setUserExercises(temp);
+        setSelectedEx(temp[0]);
       })
       .catch(error => {
         console.log(error);
@@ -55,8 +51,6 @@ const UserCharts = props => {
     })
       .then(resJson => resJson.json())
       .then(logs => {
-        // console.log({ "res": res });
-        // console.log(logs);
         let weightValues = [];
         logs.map(workout => {
           // if exercise does not have same name, null is put in place of weight
@@ -66,16 +60,14 @@ const UserCharts = props => {
             }
           });
         });
-        console.log({ "name": name });
-        console.log(weightValues);
         // remove the nulls and set state
         // return weightValues.filter(w => !w ? false : true);
         setSelectedExWeight(weightValues.filter(w => !w ? false : true));
+
       })
       .catch(error => {
         console.log(error);
         // error
-        setUserExercises([]);
       });
   }
 
@@ -86,7 +78,7 @@ const UserCharts = props => {
   const chartConfig = {
     backgroundGradientFrom: "#abd1ff",
     backgroundGradientFromOpacity: 0,
-    backgroundGradientTo: "#0",
+    backgroundGradientTo: 0,
     backgroundGradientToOpacity: 0.5,
     color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
     strokeWidth: 2, // optional, default 3
@@ -94,11 +86,17 @@ const UserCharts = props => {
   };
 
   const generateLineChartData = () => {
+    // create dummy default labels 1...n
+    let labels = []
+    for (let i = 0; i < selectedExWeight.length; i++) {
+      labels.push((i + 1).toString());
+    }
+
     let data = {
-      labels: ["1", "2"],
+      labels: labels,
       datasets: [
         {
-          data: selectedExWeight,
+          data: selectedExWeight.map(x => Number(x)),
           color: (opacity = 1) => `rgba(50, 65, 244, ${opacity})`, // optional
           strokeWidth: 3 // optional
         }
@@ -109,30 +107,35 @@ const UserCharts = props => {
 
   useEffect(() => {
     getExercises();
-  }, [])
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text style={{ top: 75, fontSize: 20, fontWeight: '600', alignSelf: 'center' }}>Select an Exercise</Text>
-      <Picker
-        selectedValue={selectedEx}
-        onValueChange={(itemValue, itemIndex) => setSelectedEx(itemValue)}
-      >
-        {generatePickerItems(userExercises)}
-      </Picker>
-      <View style={styles.button}>
-        <Button // has padding bottom to avoid collision with sign up
-          titleStyle={{ fontWeight: "bold" }}
-          // buttonStyle={{ backgroundColor: "#32a86d" }}
-          buttonStyle={{ backgroundColor: "#0bbd52" }}
-          onPress={getExerciseWeightData}
-          title="Generate Charts"
-        />
-      </View>
+      {(!!selectedEx) && (
+        <View>
+          <Picker
+            style={{ marginTop: 60 }}
+            selectedValue={selectedEx}
+            onValueChange={(itemValue, itemIndex) => setSelectedEx(itemValue)}
+          >
+            {generatePickerItems(userExercises)}
+          </Picker>
+          <View style={styles.button}>
+            <Button // has padding bottom to avoid collision with sign up
+              titleStyle={{ fontWeight: "bold" }}
+              // buttonStyle={{ backgroundColor: "#32a86d" }}
+              buttonStyle={{ backgroundColor: "#0bbd52" }}
+              onPress={getExerciseWeightData}
+              title="Generate Charts"
+            />
+          </View>
+        </View>
+      )}
       {/* this boolean might be unnecessary */}
       {(selectedExWeight.length > 0) && (
         <View style={{ alignSelf: 'center' }}>
           <LineChart
-            // data={{ data: selectedExWeight }}
             data={generateLineChartData()}
             width={Dimensions.get('window').width / 1.25} // from react-native
             height={250}

f07bce83b3a15c07f8a525e259bec1ae33985b86
Usman Siddiqui <usman98789@hotmail.com>
2020-04-12 17:32:48 -0400

    updates

--- LiftLogs/components/ar.js
+++ LiftLogs/components/ar.js
@@ -1,142 +1,162 @@
-import 'three';
+import "three";
 import { AR } from "expo";
-import ExpoGraphics from 'expo-graphics';
-import ExpoTHREE, { THREE } from 'expo-three';
-import { Camera as ARCamera, BackgroundTexture as ARbg, MagneticObject } from 'expo-three-ar';
-import React from 'react';
-import { PixelRatio, Dimensions } from 'react-native';
-
+import ExpoGraphics from "expo-graphics";
+import ExpoTHREE, { THREE } from "expo-three";
+import {
+	Camera as ARCamera,
+	BackgroundTexture as ARbg,
+	MagneticObject
+} from "expo-three-ar";
+import React from "react";
+import { PixelRatio, Dimensions } from "react-native";
 
 // much of this code adapted from Evan Bacon https://snack.expo.io/@bacon/load-simple-obj-model
 export default class Ar extends React.Component {
-    render() {
-        return (
-            <ExpoGraphics.View
-                style={{ flex: 1 }}
-                onContextCreate={this.onContextCreate}
-                isArEnabled
-                arTrackingConfiguration={AR.TrackingConfigurations.World}
-                onRender={this.onRender}
-                onResize={this.onResize}
-            />
-        );
-    }
+	constructor(props) {
+		super(props);
+	}
 
-    onContextCreate = async ({ gl }) => {
+	render() {
+		return (
+			<ExpoGraphics.View
+				key={this.props.chosen}
+				style={{ flex: 1 }}
+				onContextCreate={this.onContextCreate}
+				isArEnabled
+				arTrackingConfiguration={AR.TrackingConfigurations.World}
+				onRender={this.onRender}
+				onResize={this.onResize}
+			/>
+		);
+	}
 
-        AR.setPlaneDetection(AR.PlaneDetectionTypes.Horizontal);
+	onContextCreate = async ({ gl }) => {
+		AR.setPlaneDetection(AR.PlaneDetectionTypes.Horizontal);
 
-        // had to define these constants
-        const screenWidth = Math.round(Dimensions.get('window').width);
-        const screenHeight = Math.round(Dimensions.get('window').height);
+		// had to define these constants
+		const screenWidth = Math.round(Dimensions.get("window").width);
+		const screenHeight = Math.round(Dimensions.get("window").height);
 
-        const { drawingBufferWidth: width, drawingBufferHeight: height } = gl;
-        const scale = PixelRatio.get();
+		const { drawingBufferWidth: width, drawingBufferHeight: height } = gl;
+		const scale = PixelRatio.get();
 
-        this.renderer = new ExpoTHREE.Renderer({
-            gl,
-        });
+		this.renderer = new ExpoTHREE.Renderer({
+			gl
+		});
 
-        this.renderer.capabilities.maxVertexUniforms = 52502;
-        this.renderer.setPixelRatio(scale);
-        this.renderer.setSize(width / scale, height / scale);
-        this.renderer.setClearColor(0x000000, 1.0);
+		this.renderer.capabilities.maxVertexUniforms = 52502;
+		this.renderer.setPixelRatio(scale);
+		this.renderer.setSize(width / scale, height / scale);
+		this.renderer.setClearColor(0x000000, 1.0);
 
-        this.camera = new ARCamera(screenWidth, screenHeight, 0.001, 10000000);
-        this.camera.position.set(0, 6, 12);
-        this.camera.lookAt(0, 0, 0);
+		this.camera = new ARCamera(screenWidth, screenHeight, 0.001, 10000000);
+		this.camera.position.set(0, 6, 12);
+		this.camera.lookAt(0, 0, 0);
 
-        this.setupScene();
+		this.setupScene();
 
-        // Create a sticky node
-        // this.magneticObject = new MagneticObject();
-        // this.scene.add(this.magneticObject);
+		// Create a sticky node
+		// this.magneticObject = new MagneticObject();
+		// this.scene.add(this.magneticObject);
 
-        await this.loadModelsAsync();
-    };
+		await this.loadModelsAsync();
+	};
 
-    setupScene = () => {
-        // scene
-        this.scene = new THREE.Scene();
+	setupScene = () => {
+		// scene
+		this.scene = new THREE.Scene();
 
-        this.scene.background = new ARbg(this.renderer);
-        this.scene.fog = new THREE.FogExp2(0xcccccc, 0.002);
+		this.scene.background = new ARbg(this.renderer);
+		this.scene.fog = new THREE.FogExp2(0xcccccc, 0.002);
 
-        // this.scene.add(new THREE.GridHelper(50, 50, 0xffffff, 0x555555));
+		// this.scene.add(new THREE.GridHelper(50, 50, 0xffffff, 0x555555));
 
-        this.setupLights();
-    };
+		this.setupLights();
+	};
 
-    setupLights = () => {
-        // lights
-        const directionalLightA = new THREE.DirectionalLight(0xffffff);
-        directionalLightA.position.set(1, 1, 1);
-        this.scene.add(directionalLightA);
+	setupLights = () => {
+		// lights
+		const directionalLightA = new THREE.DirectionalLight(0xffffff);
+		directionalLightA.position.set(1, 1, 1);
+		this.scene.add(directionalLightA);
 
-        const directionalLightB = new THREE.DirectionalLight(0xffeedd);
-        directionalLightB.position.set(-1, -1, -1);
-        this.scene.add(directionalLightB);
+		const directionalLightB = new THREE.DirectionalLight(0xffeedd);
+		directionalLightB.position.set(-1, -1, -1);
+		this.scene.add(directionalLightB);
 
-        const ambientLight = new THREE.AmbientLight(0x222222);
-        this.scene.add(ambientLight);
-    };
+		const ambientLight = new THREE.AmbientLight(0x222222);
+		this.scene.add(ambientLight);
+	};
 
-    /// Magic happens here!    
-    loadModelsAsync = async () => {
-        const model = {
-            // 'mike_test_no_ss.fbx': require('../mike/mike_test_no_ss.fbx'),
-            // 'mike_test.obj': require('../mike/mike_test_2.obj'),
-            // 'mike_test.mtl': require('../mike/mike_test_2.mtl'),
-            'sci_fi5.fbx': require('../mike/sci_fi5.fbx'),
-            // 'mike.png': require('../mike/mike.png'),
-        };
+	/// Magic happens here!
+	loadModelsAsync = async () => {
+		console.log("PROPS CHOSEN", this.props.chosen);
+		let temp = "";
+		let i = 0;
+		console.log("TYPE PROP", typeof this.props.chosen);
+		// let model = { "base.fbx": require("../mike/base.fbx") };
+		if (this.props.chosen === "Stretch") {
+			console.log("In Stretch");
+			model = { "sci_fi5.fbx": require("../mike/sci_fi5.fbx") };
+			temp = "sci_fi5.fbx";
+		} else if (this.props.chosen === "Walk") {
+			model = { "base.fbx": require("../mike/base.fbx") };
+			temp = "base.fbx";
+		}
+		console.log("YOO MODEL", model);
+		// const model = {
+		// 	// 'mike_test_no_ss.fbx': require('../mike/mike_test_no_ss.fbx'),
+		// 	// 'mike_test.obj': require('../mike/mike_test_2.obj'),
+		// 	// 'mike_test.mtl': require('../mike/mike_test_2.mtl'),
+		// 	"sci_fi5.fbx": require("../mike/sci_fi5.fbx")
+		// 	// 'mike.png': require('../mike/mike.png'),
+		// };
 
-        /// Load model!
-        const obj = await ExpoTHREE.loadAsync(
-            // [model['mike_test_no_ss.fbx']],//, model['mike_test.mtl']],
-            // [model['mike_test.obj'], model['mike_test.mtl']],
-            model['sci_fi5.fbx'],
-            null,
-            name => model[name],
-        );
+		/// Load model!
+		const obj = await ExpoTHREE.loadAsync(
+			// [model['mike_test_no_ss.fbx']],//, model['mike_test.mtl']],
+			// [model['mike_test.obj'], model['mike_test.mtl']],
+			model[temp],
+			null,
+			name => model[name]
+		);
 
-        this.mixer = new THREE.AnimationMixer(obj);
-        this.action = this.mixer.clipAction(obj.animations[0]);
-        this.action.play();
+		this.mixer = new THREE.AnimationMixer(obj);
+		this.action = this.mixer.clipAction(obj.animations[i]);
+		this.action.play();
 
-        // /// Update size and position
-        ExpoTHREE.utils.scaleLongestSideToSize(obj, 3);
-        // ExpoTHREE.utils.alignMesh(obj, { x: 1, y: 1 });
+		// /// Update size and position
+		ExpoTHREE.utils.scaleLongestSideToSize(obj, 0.25);
+		// ExpoTHREE.utils.alignMesh(obj, { x: 1, y: 1 });
 
-        // set position of obj so its not huge
-        // obj.position.set(10, 0, 0);
-        obj.matrixWorld.setPosition(new THREE.Vector3(10, 0, 0));
+		// set position of obj so its not huge
+		// obj.position.set(10, 0, 0);
+		obj.matrixWorld.setPosition(new THREE.Vector3(1000, 0, 0));
 
-        // /// Smooth mesh
-        // // ExpoTHREE.utils.computeMeshNormals(mesh);
+		// /// Smooth mesh
+		// // ExpoTHREE.utils.computeMeshNormals(mesh);
 
-        // Add the mesh to the scene
-        this.scene.add(obj);
+		// Add the mesh to the scene
+		this.scene.add(obj);
 
-        // add this meshobject to the magnetic object which is already added to the scene
-        // this.magneticObject.add(obj);
-        /// Save it so we can rotate
-        // this.mesh = mesh;
-    };
+		// add this meshobject to the magnetic object which is already added to the scene
+		// this.magneticObject.add(obj);
+		/// Save it so we can rotate
+		// this.mesh = mesh;
+	};
 
-    onResize = ({ width, height }) => {
-        const scale = PixelRatio.get();
+	onResize = ({ width, height }) => {
+		const scale = PixelRatio.get();
 
-        this.camera.aspect = width / height;
-        this.camera.updateProjectionMatrix();
-        this.renderer.setPixelRatio(scale);
-        this.renderer.setSize(width, height);
-    };
+		this.camera.aspect = width / height;
+		this.camera.updateProjectionMatrix();
+		this.renderer.setPixelRatio(scale);
+		this.renderer.setSize(width, height);
+	};
 
-    onRender = delta => {
-        // this.mesh.rotation.y += 0.4 * delta;
-        this.renderer.render(this.scene, this.camera);
-        this.mixer.update(delta);
-
-    };
+	onRender = delta => {
+		// this.mesh.rotation.y += 0.4 * delta;
+		this.renderer.render(this.scene, this.camera);
+		this.mixer.update(delta);
+	};
 }

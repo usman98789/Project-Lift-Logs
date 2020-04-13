8d8783438bc5ed19e5159be5c358af8686581dbb
Usman Siddiqui <usman98789@hotmail.com>
2020-04-12 21:05:38 -0400

    ar stuff

--- LiftLogs/components/ARScreen.js
+++ LiftLogs/components/ARScreen.js
@@ -1,11 +1,33 @@
-import React from "react";
-import { View, StyleSheet, Text, SafeAreaView } from "react-native";
+import React, { useState, useEffect } from "react";
+import { View, StyleSheet, Text, SafeAreaView, Picker } from "react-native";
 import Ar from "./ar.js";
 
 const ARScreen = props => {
+	const data = ["Stretch", "Walk"];
+	const [chosen, setChosen] = useState(data[0]);
+	console.log(chosen);
 	return (
 		<SafeAreaView style={styles.container}>
-			<Ar />
+			<View style={{ height: "20%", alignSelf: "center" }}>
+				<Picker
+					selectedValue={chosen}
+					style={{ height: 50, width: 150 }}
+					onValueChange={(itemValue, itemIndex) => {
+						setChosen(itemValue);
+						return (
+							<View style={{ height: "80%" }}>
+								<Ar chosen={chosen} />
+							</View>
+						);
+					}}
+				>
+					<Picker.Item label="Stretch" value="Stretch" />
+					<Picker.Item label="Walk" value="Walk" />
+				</Picker>
+			</View>
+			<View style={{ height: "80%" }}>
+				<Ar chosen={chosen} />
+			</View>
 		</SafeAreaView>
 	);
 };
--- LiftLogs/components/ar.js
+++ LiftLogs/components/ar.js
@@ -1,81 +1,131 @@
-import React from "react";
-import { StyleSheet, Text, View, PixelRatio, Dimensions } from 'react-native';
+import "three";
 import { AR } from "expo";
-import ExpoTHREE, {THREE} from "expo-three"
-import { View as GraphicsView } from 'expo-graphics';
-import { Camera as ARCamera, BackgroundTexture as ARbg } from 'expo-three-ar';
-
+import ExpoGraphics from "expo-graphics";
+import ExpoTHREE, { THREE } from "expo-three";
+import {
+	Camera as ARCamera,
+	BackgroundTexture as ARbg,
+	MagneticObject
+} from "expo-three-ar";
+import React from "react";
+import { PixelRatio, Dimensions } from "react-native";
 
 export default class Ar extends React.Component {
-  render() {
-    return(         
-      <GraphicsView
-        style={{ flex: 1}}
-        onContextCreate={this.onContextCreate}
-        onRender={this.onRender}
-        isArEnabled
-        arTrackingConfiguration={AR.TrackingConfigurations.World}
-        // // Bonus: debug props
-        isArRunningStateEnabled
-        isArCameraStateEnabled
-      />
-    );
-  }
+	constructor(props) {
+		super(props);
+	}
 
-  onContextCreate = async ({gl, scale: PixelRatio, width, height}) => {
-    AR.setPlaneDetection(AR.PlaneDetectionTypes.Horizontal);
+	UNSAFE_componentWillMount() {
+		THREE.suppressExpoWarnings(true);
+		ExpoTHREE.THREE.suppressExpoWarnings(true);
+		console.disableYellowBox = true;
+	}
 
-    // not sure what the width and height variables are coming from and why
-    // they are so wrong
+	render() {
+		return (
+			<ExpoGraphics.View
+				key={this.props.chosen}
+				style={{ flex: 1 }}
+				onContextCreate={this.onContextCreate}
+				isArEnabled
+				arTrackingConfiguration={AR.TrackingConfigurations.World}
+				onRender={this.onRender}
+				onResize={this.onResize}
+			/>
+		);
+	}
 
-    // had to define these constants
-    const screenWidth =  Math.round(Dimensions.get('window').width);
-    const screenHeight = Math.round(Dimensions.get('window').height);
+	onContextCreate = async ({ gl }) => {
+		AR.setPlaneDetection(AR.PlaneDetectionTypes.Horizontal);
 
-    // Create a 3D renderer
-    this.renderer = new ExpoTHREE.Renderer({
-      gl,
-      PixelRatio,
-      screenWidth,
-      screenHeight,
-    });
+		const screenWidth = Math.round(Dimensions.get("window").width);
+		const screenHeight = Math.round(Dimensions.get("window").height);
 
-    // We will add all of our meshes to this scene.
-    this.scene = new THREE.Scene();
+		const { drawingBufferWidth: width, drawingBufferHeight: height } = gl;
+		const scale = PixelRatio.get();
 
-    this.camera = new ARCamera(screenWidth, screenHeight, 0.01, 1000);
-    // this.scene.background = new ThreeAR.BackgroundTexture(this.renderer);
-    this.scene.background = new ARbg(this.renderer);
+		this.renderer = new ExpoTHREE.Renderer({
+			gl
+		});
 
-    // Define our shape (Below is a tetrahedron, but can be whatever)
-    const geometry = new THREE.TetrahedronBufferGeometry(0.1, 0);
-    // Define the material, Below is material with hex color #00ff00
-    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-    // Define the full 3-D object
-    const objectToRender = new THREE.Mesh(geometry, material);
+		this.renderer.capabilities.maxVertexUniforms = 52502;
+		this.renderer.setPixelRatio(scale);
+		this.renderer.setSize(width / scale, height / scale);
+		this.renderer.setClearColor(0x000000, 1.0);
 
-    this.scene.add(objectToRender);
+		this.camera = new ARCamera(screenWidth, screenHeight, 0.001, 10000000);
+		this.camera.position.set(0, 6, 12);
+		this.camera.lookAt(0, 0, 0);
 
-    const animate = () => {      
-      requestAnimationFrame(animate);      
-      objectToRender.rotation.x += 0.01;      
-      objectToRender.rotation.y += 0.01;      
-      this.renderer.render(this.scene, this.camera);      
-      gl.endFrameEXP();    
-    };
-    animate();
+		this.setupScene();
 
-    this.scene.add(new THREE.AmbientLight(0xffffff));
-  }
+		await this.loadModelsAsync();
+	};
 
-  onRender = () => {
-    this.renderer.render(this.scene, this.camera);
-  };
+	setupScene = () => {
+		// scene
+		this.scene = new THREE.Scene();
 
-  componentDidMount() {
-    // Turn off extra warnings
-    THREE.suppressExpoWarnings(true);
-    // ExpoTHREE.suppressWarnings();
-  }
+		this.scene.background = new ARbg(this.renderer);
+		this.scene.fog = new THREE.FogExp2(0xcccccc, 0.002);
 
+		this.setupLights();
+	};
+
+	setupLights = () => {
+		const directionalLightA = new THREE.DirectionalLight(0xffffff);
+		directionalLightA.position.set(1, 1, 1);
+		this.scene.add(directionalLightA);
+
+		const directionalLightB = new THREE.DirectionalLight(0xffeedd);
+		directionalLightB.position.set(-1, -1, -1);
+		this.scene.add(directionalLightB);
+
+		const ambientLight = new THREE.AmbientLight(0x222222);
+		this.scene.add(ambientLight);
+	};
+
+	loadModelsAsync = async () => {
+		let temp = "";
+		let i = 0;
+		if (this.props.chosen === "Stretch") {
+			model = { "sci_fi5.fbx": require("../FBX/sci_fi5.fbx") };
+			temp = "sci_fi5.fbx";
+		} else if (this.props.chosen === "Walk") {
+			model = { "base.fbx": require("../FBX/base.fbx") };
+			temp = "base.fbx";
+		}
+
+		const obj = await ExpoTHREE.loadAsync(
+			model[temp],
+			null,
+			name => model[name]
+		);
+
+		this.mixer = new THREE.AnimationMixer(obj);
+		this.action = this.mixer.clipAction(obj.animations[i]);
+		this.action.play();
+
+		if (this.props.chosen === "Walk") obj.rotation.x += 11;
+		ExpoTHREE.utils.scaleLongestSideToSize(obj, 0.25);
+		obj.scale.multiplyScalar(1.5);
+		0;
+		obj.matrixWorld.setPosition(new THREE.Vector3(1000, 0, 0));
+
+		this.scene.add(obj);
+	};
+
+	onResize = ({ width, height }) => {
+		const scale = PixelRatio.get();
+
+		this.camera.aspect = width / height;
+		this.camera.updateProjectionMatrix();
+		this.renderer.setPixelRatio(scale);
+		this.renderer.setSize(width, height);
+	};
+
+	onRender = delta => {
+		this.renderer.render(this.scene, this.camera);
+		this.mixer.update(delta);
+	};
 }
